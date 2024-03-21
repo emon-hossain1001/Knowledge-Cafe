4dd3bfe9d6608b6052acc32208a77e33cb329c13
emon-hossain1001 <mrx00001001@gmail.com>
2024-03-21 14:34:08 +0600

    update bookmarks

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -16,7 +16,7 @@ const Blog = ({blog, handleAddToBookmark}) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
-                    <button onClick={handleAddToBookmark} className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
@@ -30,7 +30,9 @@ const Blog = ({blog, handleAddToBookmark}) => {
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func.isRequired
+
 }
 
 export default Blog;
